c23d60d4e5f807e91ede90db6be4005cfdc4c77e
Matteo Ferrando <matteo.ferrando2@gmail.com>
2020-06-08 21:17:04 -0400

    use logo as back button

--- src/ClusterBase.js
+++ src/ClusterBase.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import logo from './assets/logo.png';
 import './Cluster.css';
 
 function ClusterBase(board) {
+  const history = useHistory();
   return (
     <div className="cluster">
       {board}
-      <a className="Home-logo-header" href="/">
+      <a className="Home-logo-header" href="/" onClick={() => history.goBack()}>
         <img src={logo} className="Home-logo" alt="logo" />
       </a>
     </div>
