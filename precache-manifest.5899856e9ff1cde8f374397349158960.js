62a2335f21f4eaad4d9bd82c9bc980a08eb1319a
Matteo Ferrando <matteo.ferrando2@gmail.com>
2020-06-09 23:24:06 -0400

    Updates

---
+++ precache-manifest.5899856e9ff1cde8f374397349158960.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb337ebda73572e473d0afea115246b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a33f5327e500efd1aff3",
+    "url": "/static/css/main.07ee92bd.chunk.css"
+  },
+  {
+    "revision": "54af642497b403b3eafc",
+    "url": "/static/js/2.cfb93801.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cfb93801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a33f5327e500efd1aff3",
+    "url": "/static/js/main.97d1b183.chunk.js"
+  },
+  {
+    "revision": "fa66eab5b10555bd2a6a",
+    "url": "/static/js/runtime-main.bbd28c88.js"
+  },
+  {
+    "revision": "6bcf138c8415dc675a318bfe1865f978",
+    "url": "/static/media/Cluster-1_2000.6bcf138c.jpg"
+  },
+  {
+    "revision": "04e827f50968403ba75c17cfec360304",
+    "url": "/static/media/Cluster-2_2000.04e827f5.jpg"
+  },
+  {
+    "revision": "cc4786925ce53c8c6b2542a29a386cc3",
+    "url": "/static/media/Cluster-3_2000.cc478692.jpg"
+  },
+  {
+    "revision": "b11af815d17989f78b7723eaeea9405c",
+    "url": "/static/media/Cluster-3_video.b11af815.mp4"
+  },
+  {
+    "revision": "464b8934690cf41c78a594516f582c27",
+    "url": "/static/media/Cluster-4_2000.464b8934.jpg"
+  },
+  {
+    "revision": "7a95d7d62cb4ab5f77e505f5a3079427",
+    "url": "/static/media/Cluster-5_2000.7a95d7d6.jpg"
+  },
+  {
+    "revision": "e0630a575b5345d3bf0c2e1df96c79f8",
+    "url": "/static/media/Cluster-6_2000.e0630a57.jpg"
+  },
+  {
+    "revision": "d80e0e1c250af459878dae3fcfccefdb",
+    "url": "/static/media/Cluster-6_switch.d80e0e1c.gif"
+  },
+  {
+    "revision": "29435f902db741e090ae11e49af684af",
+    "url": "/static/media/Plau-Tenez-Italic.29435f90.otf"
+  },
+  {
+    "revision": "6e115fe45d7622f9779b9d898d6ce12e",
+    "url": "/static/media/Plau-Tenez-Regular.6e115fe4.otf"
+  },
+  {
+    "revision": "368ced64281c5728be8d707c69c88b77",
+    "url": "/static/media/logo.368ced64.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/edg-studio/precache-manifest.7e7ff9f188082441d2f73ef152f88253.js"
+  "/precache-manifest.5899856e9ff1cde8f374397349158960.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/edg-studio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
